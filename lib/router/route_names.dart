6e7622e1e74ec30aeebf8a26fdd2b39fb1732ea7
pacblinky <40526207+pacblinky@users.noreply.github.com>
2026-03-07 10:40:31 +0200

    added navigation service and routes stuff

---
+++ lib/router/route_names.dart
@@ -0,0 +1,9 @@
+class RouteNames {
+  RouteNames._();
+
+  static const String splash = 'splash';
+  static const String login = 'login';
+  static const String register = 'register';
+
+  static const String home = 'home';
+}
