43acef3133060f4a7caec96d44a99d5537549e51
islamsaeed9854-ui <islamsaeed9854@gmail.com>
2026-03-07 02:49:52 +0200

    added router

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'presentation/router/app_router.dart';
+import 'router/app_router.dart';
 
 class OtlobApp extends StatelessWidget {
   const OtlobApp({super.key});
---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:injectable/injectable.dart';
+
+@singleton
+class AppRouter {
+  static const String homePath = '/';
+  static const String loginPath = '/login';
+
+  late final GoRouter _router = GoRouter(
+    initialLocation: homePath,
+    routes: [
+      GoRoute(
+        path: homePath,
+        name: 'home',
+        builder: (context, state) =>
+            const Scaffold(body: Center(child: Text('Home'))),
+      ),
+      GoRoute(
+        path: loginPath,
+        name: 'login',
+        builder: (context, state) =>
+            const Scaffold(body: Center(child: Text('Login'))),
+      ),
+    ],
+    errorBuilder: (context, state) =>
+        const Scaffold(body: Center(child: Text('404'))),
+  );
+
+  GoRouter get router => _router;
+}
