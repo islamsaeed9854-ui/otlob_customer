771540b60dbe82b221afbb5a5629113c0590e47a
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-03-24 22:09:51 +0200

    added onboarding routes

--- lib/core/router/route_paths.dart
+++ lib/core/router/route_paths.dart
@@ -2,6 +2,7 @@ class RoutePaths {
   RoutePaths._();
 
   static const String splash = '/splash';
+  static const String onboarding = '/onboarding';
   static const String login = '/login';
   static const String register = '/register';
 
